85884619ab62fcbc0da6818f5e9af294ba37f329
daanhvc <daan.vanhemert@hvc.nl>
2023-11-24 14:09:10 +0100

    Update H1O22.js

--- JS/H1/H1O22.js
+++ JS/H1/H1O22.js
@@ -18,22 +18,23 @@ function draw() {
   mouseY=constrain(mouseY,25,height - 25);
 
   text("Beweeg de muis | x = " + round(mouseX) + " en y = " + round(mouseY),10,20);
+
+
   
-  if (mouseX >= width - 30 || mouseY >= height - 30 || mouseX >= width +30 ) {
+ 
+  if (mouseX >= width - 30 || mouseY >= height - 30 || mouseX <= 30  || mouseY <= 30  ) {
     kleur='red';
   }
   else {
     kleur='indianred';
   }
   
- 
-   
-  
+
  
   
   ellipse(width / 2,height / 2,100);
   tekenJos(mouseX,mouseY,kleur);
-  afstand=dist(315,105,225,225);
+  afstand=dist(mouseX,mouseY,width / 2,height /2 );
   text(afstand,10,40);
 }
 

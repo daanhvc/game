cac645f9ab57ec384594f60cd2ef3b4cf85e42d4
daanhvc <145122245+daanhvc@users.noreply.github.com>
2023-12-08 12:45:07 +0100

    f

--- JS/H1/H1O25.js
+++ JS/H1/H1O25.js
@@ -1,84 +1,59 @@
-var straalWit = 20;
-var xPositieWit;
-var yPositieWit;
-var xSnelheidWit = 15;
-var ySnelheidWit = 10;
-var stuiterFactor = 1.5; // Verhoogde stuiteringsfactor
-
-var straalBlauw = 20;
-var xPositieBlauw;
-var yPositieBlauw;
+var straal = 0x14;
+var xPositie;
+var yPositie;
+var xSnelheid = 0x8;
+var ySnelheid = 0x5;
+var onderlingeAfstand;
 
 function setup() {
-  canvas = createCanvas(1000, 300);
-  canvas.parent('processing');
-  frameRate(50);
-  colorMode(RGB, 255, 255, 255, 1);
-  background(0, 0, 75, 1);
-  noStroke();
-  textFont("Verdana");
-  textSize(20);
-  xPositieWit = straalWit;
-  yPositieWit = height / 2;
- 
-  xPositieBlauw = width / 2;
-  yPositieBlauw = height - straalBlauw;
+    var canvas = createCanvas(0x3e8, 0x12c);
+    canvas.parent('processing');
+    frameRate(0x32);
+    colorMode(RGB, 0xff, 0xff, 0xff, 0x1);
+    background(0x0, 0x0, 0x4b, 0x1);
+    noStroke();
+    textFont('Verdana');
+    textSize(0x8c);
+    xPositie = straal;
+    yPositie = height / 0x2;
 }
 
 function draw() {
-  background(0, 0, 75, 0.05);
- 
-  // Blauw balletje
-  fill(0, 0, 255, 1);
-  ellipse(xPositieBlauw, yPositieBlauw, 2 * straalBlauw);
- 
-  // Witte bewegende bal
-  fill(255, 255, 255, 1);
-  ellipse(xPositieWit, yPositieWit, 2 * straalWit);
-
-  // Onderlinge afstand berekenen met de dist-functie
-  var onderlingeAfstand = dist(xPositieWit, yPositieWit, xPositieBlauw, yPositieBlauw);
-
-  if (onderlingeAfstand <= straalWit + straalBlauw) {
-    eindScherm();
-    noLoop();
-  }
-
-  gebruikBesturing();
-
-  // Aantrekkingskracht van zijkanten
-  if (xPositieWit < width / 2) {
-    xSnelheidWit -= 0.5 * stuiterFactor; // Keer de aantrekkingskracht om en verhoog de stuiteringsfactor
-  } else {
-    xSnelheidWit += 0.5 * stuiterFactor; // Keer de aantrekkingskracht om en verhoog de stuiteringsfactor
-  }
-
-  // Harde terugstuitering bij de zijkanten voor de witte bal
-  if (xPositieWit < straalWit || xPositieWit > width - straalWit) {
-    xSnelheidWit *= -1 * stuiterFactor; // Verhoog de stuiteringsfactor voor snelheid
-  }
-
-  if (yPositieWit < straalWit || yPositieWit > height - straalWit) {
-    ySnelheidWit *= -1 * stuiterFactor; // Verhoog de stuiteringsfactor voor snelheid
-  }
- 
-  // Beweging witte bal
-  xPositieWit += xSnelheidWit;
-  yPositieWit += ySnelheidWit;
+    background(0x0, 0x0, 0x4b, 0.05);
+    fill(0x0, 0x0, 0xff, 0x1);
+    ellipse(0x226, height - straal, 0x2 * straal);
+    xPositie += xSnelheid;
+    yPositie += ySnelheid;
+    fill(0xff, 0xff, 0xff, 0x1);
+    ellipse(xPositie, yPositie, 0x2 * straal);
+    onderlingeAfstand = round(dist(0x226, height - straal, xPositie, yPositie)) - 0x2 * straal;
+    if (onderlingeAfstand <= 0x0) {
+        eindScherm();
+        noLoop();
+    }
+    gebruikBesturing();
+    if (yPositie < straal || yPositie > height - straal) {
+        ySnelheid *= -0x1;
+    }
+    if (xPositie < straal || xPositie > width - straal) {
+        xSnelheid *= -0x1;
+    }
 }
 
 function gebruikBesturing() {
-  if (keyIsDown(LEFT_ARROW)) {
-    xSnelheidWit += 0.5 * stuiterFactor; // Keer de aantrekkingskracht om en verhoog de stuiteringsfactor
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xSnelheidWit -= 0.5 * stuiterFactor; // Keer de aantrekkingskracht om en verhoog de stuiteringsfactor
-  }
+    if (keyIsDown(LEFT_ARROW)) {
+        xSnelheid += 0x1;
+        ySnelheid += 0x1;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        xSnelheid -= 0x1;
+        ySnelheid -= 0x1;
+    }
 }
 
 function eindScherm() {
-  background('white');
-  fill('black');
-  text("GEPAKT!", width/2 - 50, height/2);
-  noLoop();
+    background('white');
+    fill('black');
+    text('GEVANGEN!', 0x4b, 0xc8);
+    noLoop();
 }

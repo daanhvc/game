b70713d05f17926fd14c57651c7c22d880e9ad91
daanhvc <145122245+daanhvc@users.noreply.github.com>
2024-04-05 12:47:53 +0200

    Update H3O01.js

--- JS/H3/H3O01.js
+++ JS/H3/H3O01.js
@@ -1,81 +1,87 @@
-class Bal {
-  constructor(x,y) {
-  this.diameter = 40;
-  this.straal = 20;
-  this.x = x;
-  this.y = y;
-  this.R = random(0,255);
-  this.G = random(0,255);
-  this.B = random(0,255);
-  this.alpha = 0.8;
-  this.snelheid = 0;
-  this.aantalKerenGestuiterd = 0;
-  this.geraakt = false;
-  }
-  
-  beweeg() {
-    this.snelheid+=9.81/15;
-    this.y+=this.snelheid;
-    if (this.y>=canvas.height-this.straal) {
-      this.snelheid*=-1;
-      this.y=canvas.height-this.straal;
-      this.aantalKerenGestuiterd++;
-    }
-  }
-  
-  teken() {
-    push();
-    noStroke();
-    fill(this.R,this.G,this.B,this.alpha);
-    ellipse(this.x,this.y,this.diameter);
-    pop();
-  }
-}
-
-var ballenArray = [];
-var erIsNogGeenBalGestuiterd = true;
 var score = 0;
+var balletjes = [];
+var ballSize = 50;
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(600, 450);
   canvas.parent('processing');
-  colorMode(RGB,255,255,255,1);
-  frameRate(15);
   textFont("Verdana");
-  textSize(24);
-  ballenArray[0] = new Bal(225,50);
+  textSize(14);
 
+  // genereer 4 balletjes bij laden pagina
+  for (var i = 0; i < 4; i++) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+
+  // maak knop voor opnieuw beginnen
+  resetButton = createButton('Opnieuw beginnen');
+  resetButton.position(620, 180);
+  resetButton.mousePressed(resetGame);
 }
 
 function draw() {
-  background('salmon');
-  text("N:"+ballenArray.length+" score:"+score,5,24);
-  if (mouseIsPressed && erIsNogGeenBalGestuiterd) {
-    ballenArray.push(new Bal(mouseX,mouseY));
+  background('lavender');
+  fill('black');
+
+  text("Score: " + score, 400, 20);
+
+  // teken alle balletjes
+  for (var i = 0; i < balletjes.length; i++) {
+    push();
+    translate(balletjes[i].x, balletjes[i].y);
+    fill(balletjes[i].kleur);
+    ellipse(0, 0, ballSize);
+    pop();
   }
-  
-  for (b=0;b<ballenArray.length;b++) {
-    ballenArray[b].beweeg();
-    ballenArray[b].teken();
-    if (ballenArray[b].aantalKerenGestuiterd==1) {
-      erIsNogGeenBalGestuiterd = false;
-      if (dist(mouseX,mouseY,ballenArray[b].x,ballenArray[b].y)<=ballenArray[b].straal && mouseIsPressed && ballenArray[b].geraakt==false) {
-        score++;
-        ballenArray[b].geraakt = true;
-        ballenArray[b].alpha = 0;
+
+  // genereer een nieuw balletje als er geen balletjes meer is
+  if (balletjes.length < 4) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+
+  // restart the game if the ball size becomes 0
+  if (ballSize == 0) {
+    resetGame();
+  }
+}
+
+function resetGame() {
+  score = 0;
+  balletjes = [];
+  ballSize = 50;
+
+  // genereer 4 nieuwe balletjes
+  for (var i = 0; i < 4; i++) {
+    balletjes.push({
+      x: random(50, width - 50),
+      y: random(50, height - 50),
+      kleur: color(random(255), random(255), random(255))
+    });
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < balletjes.length; i++) {
+    if (dist(mouseX, mouseY, balletjes[i].x, balletjes[i].y) < ballSize / 2) {
+      score++;
+      if (score % 50 == 0) {
+        ballSize -= 5;
       }
-    }
-    if (ballenArray[b].aantalKerenGestuiterd==2) {
-      if (ballenArray.length==score) {
-        background('green');
-        text("Het is je gelukt! Je score is "+score+" :)",5,24);
-        noLoop();
-      }
-      else {
-        background('red');
-        text("Helaas. Je hebt verloren :(",5,24);
-        noLoop();
-      }
+      balletjes.splice(i, 1);
     }
   }
+}
+
+function balletje(x, y, kleur) {
+  this.x = x;
+  this.y = y;
+  this.kleur = kleur;
 }
